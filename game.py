9d296666e14142dbd3353a083ad2f8256b121b3f
isNikol4s <nikolassc4mpos@gmail.com>
2023-11-10 11:37:05 -0300

    Criei uma função para niveis

--- game.py
+++ game.py
@@ -1,14 +1,10 @@
 from tabela import tabela
 from temp import temporizador
+from niveis import niveis
 import os
 
 os.system('cls')
 print(tabela)
 nivel = input('Digite o código: ')
 
-if nivel == '1':
-    temporizador(50)
-elif nivel == '2':
-    temporizador(25)
-elif nivel == '3':
-    temporizador(15)
+print(niveis(nivel))
---
+++ niveis.py
@@ -0,0 +1,9 @@
+from temp import temporizador
+
+def niveis(num):
+    if num == '1':
+        temporizador(50)
+    elif num == '2':
+        temporizador(25)
+    elif num == '3':
+        temporizador(15)
